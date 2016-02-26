c36db7f691ea54fbbb48bf510df94d7f0d5cfdca
Iain Mathieson <iain_mathieson@hms.harvard.edu>
2016-02-26 15:42:34 -0500

    oo

--- mutation_spectrum.py
+++ mutation_spectrum.py
@@ -162,6 +162,7 @@ def main(options):
             het_count=sum([g in hetgts for g in gts])
             hom_count=sum([g==mutgt for g in gts])
             total_count=het_count+2*hom_count
+
             if total_count==options["count"]:
                 counted+=1
                 which_is_het= [i for i, x in enumerate(gts) if x in hetgts]
